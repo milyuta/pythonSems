b3a09a029211a463786d266b3702f0662cbb2d0e
milyuta <mil@mail.ry>
2022-11-28 21:08:00 +0300

    44hw done

--- 4HW/4HW.py
+++ 4HW/4HW.py
@@ -53,7 +53,8 @@
 
 
 
-# 32. Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+# 32. Задайте последовательность чисел. Напишите программу, 
+# которая выведет список неповторяющихся элементов исходной последовательности.
 
 # import random
 # n = int(input('Input the number: '))
@@ -88,21 +89,22 @@
 ##### 34. *Даны два файла, в каждом из которых находится запись многочлена. Задача - сформировать файл, содержащий сумму многочленов.
 # 2x² + 4x + 5 = 0 и x² + 5x + 3 = 0 => 3x² + 9x + 8 = 0
 
-# import re
+import re
 
-# file_1 = open('1polinomial.txt', 'r', encoding='utf8')
-# polinomial1 = file_1.read()
+file_1 = open('1polinomial.txt', 'r', encoding='utf8')
+polinomial1 = file_1.read()
 
-# p1_1,p1_2,p1_3,p1_4,p1_5,p1_6,p1_7,p1_8,p1_9,p1_10,p1_11=re.split('[+*x=0,]', polinomial1)
-# file_1.close()
+p1_1,p1_2,p1_3,p1_4,p1_5,p1_6,p1_7,p1_8,p1_9,p1_10,p1_11=re.split('[+*x=0,]', polinomial1)
+file_1.close()
 
-# file_2 = open('2polinomial.txt', 'r', encoding='utf8')
-# polinomial2 = file_2.read()
-# p2_1,p2_2,p2_3,p2_4,p2_5,p2_6,p2_7,p2_8,p2_9,p2_10,p2_11=re.split('[+*x=0,]', polinomial2)
-# file_2.close()
+file_2 = open('2polinomial.txt', 'r', encoding='utf8')
+polinomial2 = file_2.read()
+p2_1,p2_2,p2_3,p2_4,p2_5,p2_6,p2_7,p2_8,p2_9,p2_10,p2_11=re.split('[+*x=0,]', polinomial2)
+file_2.close()
 
-# res_polinomial=(f'{int(p1_1)+int(p2_1)}*x**2+{int(p1_6)+int(p2_6)}*x+{int(p1_9)+int(p2_9)}=0')
+print(res_polinomial)
+res_polinomial=(f'{int(p1_1)+int(p2_1)}*x**2+{int(p1_6)+int(p2_6)}*x+{int(p1_9)+int(p2_9)}=0')
 
-# with open('res_polinomial.txt', 'w', encoding='utf8') as out:
-#     out.writelines(' '.join(res_polinomial))
+with open('res_polinomial.txt', 'w', encoding='utf8') as out:
+    out.writelines(' '.join(res_polinomial))
 
